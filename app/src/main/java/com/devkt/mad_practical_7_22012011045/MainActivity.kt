9d6cdae4ae0b2ba865d1ec426e2341bbea30a5c5
KirtanPatel8 <kirtansantoki12@gmail.com>
2024-12-01 21:31:10 +0530

    Initial commit

---
+++ app/src/main/java/com/devkt/mad_practical_7_22012011045/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.devkt.mad_practical_7_22012011045
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var personAdapter: PersonAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn=findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        fetchPersonDetails()
+    }
+    private fun fetchPersonDetails() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val data = HttpRequest().makeServiceCall(
+                    "https://api.json-generator.com/templates/iqcfRY20uFzp/data",
+
+                    )
+                withContext(Dispatchers.Main) {
+                    if (data != null) {
+                        getPersonDetailsFromJson(data)
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun getPersonDetailsFromJson(data: String) {
+        try {
+            val jsonArray = JSONArray(data)
+            val personList = Array(jsonArray.length()) { index ->
+                val jsonObject = jsonArray.getJSONObject(index)
+                Person.fromJson(jsonObject)
+            }
+            personAdapter = PersonAdapter(personList)
+            recyclerView.adapter = personAdapter
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
